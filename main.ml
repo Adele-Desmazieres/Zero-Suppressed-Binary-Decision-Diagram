343dca8f2293d55428776c6a9020c709a12a69a4
Delta-Chat <adele.desmazi@gmail.com>
2023-11-04 10:13:56 +0100

    Better tests

--- main.ml
+++ main.ml
@@ -56,14 +56,18 @@ let rec insert x b =
   | e::b2 -> e::(insert x b2)
 ;;
 
-let rec print_bigint b =
-  match b with
-  | [] -> print_string "EMPTY"
-  | [e] -> Printf.printf "%s\n" (to_string e)
-  | e::b2 ->
-      print_string (to_string e);
-      print_string "; ";
-      print_bigint b2
+let print_bigint b =
+  let rec print_bigint_aux b = 
+    match b with
+    | [] -> print_string "EMPTY"
+    | [e] -> printf "%s]\n" (to_string e)
+    | e::b2 ->
+        print_string (to_string e);
+        print_string "; ";
+        print_bigint_aux b2
+  in 
+  printf "[";
+  print_bigint_aux b
 ;;
 
 let rec bigint_to_string b =
@@ -466,7 +470,6 @@ let rec compressionParArbreAux current_node adv =
       let g_feuilles = liste_feuilles g in
       let d_feuilles = liste_feuilles d in
       
-      
       (* Règle Z *)
       if composition d_feuilles = [0L] 
       then 
@@ -614,16 +617,84 @@ print_arbre b_bdd;;
 printf "\n\n";;
 *)
 
-let b2 = [0L; 1L];;
-let () = bigintToDot "bdd_0L_1L" b2 true;;
 
-let b3 = [9223372036854775808L; 1L];;
-let () = bigintToDot "bdd_2pow63_1L" b3 true;;
+(* TESTS PARTIE 4 *)
+(* 
+  Pour tester :
+  1. Génération des fichiers dot :
+      $ ocaml main.ml
+  2. Vérification de l'égalité des arbres compressés par listeDejaVus et par arbreDejaVus :
+      $ diff e3_bits_LDV_compressed.dot e3_bits_ADV_compressed.dot
+*)
 
-(* let () = bigintToDot "ARBRE_bdd_2pow63_1L" b3 false;; *)
+let b = [7L];; (* = 7 *)
+print_bigint b;;
+let () = bigintToDot "7L_LDV" b true;;
+let () = bigintToDot "7L_ADV" b false;;
+printf "\n"
 
-let a = [7L];;
-let () = bigintToDot "ARBRE_7L" a false;;
+let b = [0L; 1L];; (* = 2^64 *)
+print_bigint b;;
+let () = bigintToDot "0L_1L_LDV" b true;;
+let () = bigintToDot "0L_1L_ADV" b false;;
+printf "\n"
 
+let b = [9223372036854775808L; 1L];; (* = 2^63 + 2^64 *)
+print_bigint b;;
+let () = bigintToDot "2pow63_1L_LDV" b true;;
+let () = bigintToDot "2pow63_1L_ADV" b false;;
+printf "\n"
+
+let b = [-4077520903251697734L; 3725098519587869504L];; (* = 68715939040191755864311453423018172346 *)
+print_bigint b;;
+let () = bigintToDot "126bits_LDV" b true;;
+let () = bigintToDot "126bits_ADV" b false;;
+printf "\n"
+
+let b = genalea 1000L;;
+print_bigint b;;
+let () = bigintToDot "e3_bits_LDV" b true;;
+let () = bigintToDot "e3_bits_ADV" b false;;
+printf "\n"
+
+let b = genalea 2000L;; 
+print_bigint b;;
+let () = bigintToDot "2e3_bits_LDV" b true;;
+let () = bigintToDot "2e3_bits_ADV" b false;;
+printf "\n"
+
+(* Les fichiers dots des arbres compressés ne sont pas identiques... *)
+let b = genalea 3000L;;
+print_bigint b;;
+let () = bigintToDot "3e3_bits_LDV" b true;;
+let () = bigintToDot "3e3_bits_ADV" b false;;
+printf "\n"
+
+(* Le test suivant met une 20aine de secondes à générer les dots. *)
+(* Les fichiers dots des arbres compressés ne sont pas identiques... *)
+(* 
+let b = genalea 10000L;;
+print_bigint b;;
+let () = bigintToDot "e4_bits_LDV" b true;;
+let () = bigintToDot "e4_bits_ADV" b false;;
+printf "\n"
+*)
+
+(* Compressions différentes pour, par exemple, le bigint suivant (sur 2999 bits) : 
+
+[-7397863826597483745; -7770261156321738754; 1236646854865449075; -5190928585146045899; 
+-4992808975050723075; -5741163498365487965; 7946033078052947069; -2800735776790153635; 
+-3918936533814437455; -8998001415999952448; -6294537310539991044; -7668792850197517654; 
+8563250432377705112; -552875915331482884; -6668039009565253450; -881992044850089135; 
+-5531170330478518348; -3552442076188947852; 5565918363508105768; -9161973353537052590; 
+-5502829082253087124; -3043477099894200854; 4834271741461236247; 3176104119376014797; 
+8173290540801592168; -4800686476214529944; 2343392459435193004; -7177155205930299855; 
+8863579622411367543; 7547174846486908704; 7196820598621166457; 1761421479576635729; 
+-6805764409314456464; -8737641954624191063; -3284744692263401815; 1879820879237403955; 
+-1170213225930205699; -5764086671883100520; -6879513556585695580; -4857462984785588336; 
+6258772459160726337; -1049649545095147934; -8986565433282590063; 3399141512103088421; 
+-1858961850604665463; 7974041216154821691; 31402785079399147]
+
+*)
 
 print_string "Done.\n";;
