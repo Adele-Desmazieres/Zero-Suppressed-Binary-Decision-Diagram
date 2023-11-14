f5915e2bd319a0b71972f248bb8e48ef065b8107
Delta-Chat <adele.desmazi@gmail.com>
2023-11-14 02:03:30 +0100

    Cleaner test functions

--- main.ml
+++ main.ml
@@ -529,200 +529,134 @@ let bigintToDot filename b isParListe =
 ;;
 
 
-(* TESTS PARTIES 1 ET 2 *)
+
+(* TESTS DU PROJET *)
+
 (*
-let b = [23L; 6L];;
-let b = insert 4L b;;
-print_bigint b;;
-let e = peek b;;
-let (e, b) = pop b;;
-print_bigint b;;
-
-let b2 = [38L];;
-decomposition b2;;
-let b3 = [0L;38L];;
-decomposition b3;;
-
-let c2 = [pow2 2L];;
-decomposition c2;;
-
-list_of_pow 63L;;
-list_of_pow 100L;;
-list_of_pow 36L;;
-
-completion [false; true; true; false; false; true] 4;;
-completion [false; true; true; false; false; true] 8;;
-
-composition [false; true; true; false; false; true];;
+let tests () = 
+  
+    ()
+;;
 *)
+
+let print_bool_list l =
+  let rec pblaux l =
+    match l with 
+    | [] -> ()
+    | [e] -> printf "%b" e
+    | e::l2 -> printf "%b, " e; pblaux l2
+  in
+  printf "[";
+  pblaux l;
+  printf "]\n"
+;;
+
+
+(* TESTS PARTIES 1 ET 2 *)
+let tests_1_2 () =
+  let b = [23L; 6L] in
+  let b = insert 4L b in
+  print_bigint b;
+  let e = peek b in
+  printf "%Lu" e;
+  let (e, b) = pop b in
+  print_bigint b;
+    
+  let b2 = [38L] in 
+  print_bool_list (decomposition b2);
+  
+  let b3 = [0L;38L] in 
+  print_bool_list (decomposition b3);
+  
+  let c2 = [pow2 2L] in 
+  print_bool_list (decomposition c2);
+  
+  print_bigint (list_of_pow 63L);
+  print_bigint (list_of_pow 100L);
+  print_bigint (list_of_pow 36L);
+  
+  print_bool_list (completion [false; true; true; false; false; true] 4);
+  print_bool_list (completion [false; true; true; false; false; true] 8);
+  
+  print_bigint (composition [false; true; true; false; false; true])
+;;
 
 
 (* TESTS PARTIE 3 *)
-
-let n50 = Leaf(false);;
-let n51 = Leaf(true);;
-let n41 = Node(n50, 4, n51);;
-let n42 = Node(n51, 4, n51);;
-let n31 = Node(n42, 3, n41);;
-let n32 = Node(n41, 3, n51);;
-let n33 = Node(n51, 3, n51);;
-let n21 = Node(n31, 2, n41);;
-let n22 = Node(n33, 2, n32);;
-let n1 = Node(n21, 1, n22);;
-
-(* let n3 = n2;; *)
-(* let n4 = n2;; *)
-(* Printf.printf "fils égaux ? %b\n" (n3 == n4);; *)
-(* Printf.printf "ref fils égales ? %b\n" (ref n3 = ref n4);; *)
-(* let n1 = Node(ref n3, 1, ref n4);; *)
-(* print_string (toStringDotFormat n1);; *)
-
-let print_ldv ldv =
-  print_string "\nldv3 = ";
-  List.iter (fun (a, b) ->
-      print_string "(";
-      print_bigint a;
-      print_string "";
-      let () = match (!b) with
-        | Leaf(e) -> print_string (string_of_bool e)
-        | Node(a1, e, a2) -> print_int e in
-      print_string "), " )
-    ldv
+let tests_3 () =
+  let n50 = Leaf(false) in
+  let n51 = Leaf(true) in
+  let n41 = Node(n50, 4, n51) in
+  let n42 = Node(n51, 4, n51) in
+  let n31 = Node(n42, 3, n41) in
+  let n32 = Node(n41, 3, n51) in
+  let n33 = Node(n51, 3, n51) in
+  let n21 = Node(n31, 2, n41) in
+  let n22 = Node(n33, 2, n32) in
+  let n1 = Node(n21, 1, n22) in
+  
+  print_string (toStringDotFormat n1);
+  
+  let vertable = decomposition [25899L] in
+  print_bool_list vertable;
+  
+  let a4 = cons_arbre vertable in
+  let a4c = compressionParListe a4 in
+  print_string (toStringDotFormat a4);
+  print_string (toStringDotFormat a4c);
+  printf "\n";
+  
+  let b = [-4077520903251697734L; 3725098519587869504L] in (* = 68715939040191755864311453423018172346 *)
+  print_bigint b;
+  bigintToDot "130_bits_LDV" b true
 ;;
 
-(*
-(* let a3 = cons_arbre [true; true; true; false];; *)
-let a3 = cons_arbre [true; true; false; true; false; true; false; false; true; false; true; false; false; true; true; false];;
-let a3_str = toStringDotFormat a3;;
-(* print_string a3_str;; *)
-let a3c = compressionParListe a3;;
-let a3c_str = toStringDotFormat a3c;;
-print_string a3c_str;;
-
-exportDot "BDD_25899.dot" a3_str;;
-exportDot "BDD_25899_compressed.dot" a3c_str;;
-
-let vertable = decomposition [25899L];;
-let () = List.iter (printf "%b ") vertable;;
-print_string "\n";;
-
-let a4 = cons_arbre vertable;;
-let a4c = compressionParListe a4;;
-exportDot "BDD_25899_bis.dot" (toStringDotFormat a4);;
-exportDot "BDD_25899_compressed_bis.dot" (toStringDotFormat a4c);;
-*)
-
-(* let b = genalea 130L;; *)
-(* b = [-x1L, x2L, -x3L]
-     = 1 * (2^64 - x1L) + 2^64 * x2L + 2^128 * (2^64 - x3L)
-*)
-(*
-let b = [-4077520903251697734L; 3725098519587869504L];; (* = 68715939040191755864311453423018172346 *)
-print_bigint b;;
-printf "\n";;
-let () = bigintToDot "TEST_130bits" b true;;
-
-let table = decomposition b;; (* decomposition correcte *)
-let () = List.iter (fun x -> if x then printf "true; " else printf "false; ") table;;
-printf "\n\n";;
-
-let b_bdd = cons_arbre table;;
-print_arbre b_bdd;;
-printf "\n\n";;
-*)
-
-
+  
 (* TESTS PARTIE 4 *)
-(*
-  Pour tester :
-  1. Génération des fichiers dot :
-      $ ocaml main.ml
-  2. Vérification de l'égalité des arbres compressés par listeDejaVus et par arbreDejaVus :
-      $ diff e3_bits_LDV_compressed.dot e3_bits_ADV_compressed.dot
-*)
+let tests_4 () = 
+  
+  let dirname = "./tests_4/" in
+  if not (Sys.file_exists dirname) then Sys.mkdir dirname 0o755;
+  
+  let standard_test_4 b filename =
+    print_bigint b;
+    bigintToDot (dirname ^ filename ^ "_LDV") b true;
+    bigintToDot (dirname ^ filename ^ "_ADV") b false;
+    printf "\n"
+  in
+  
+  standard_test_4 [25899L] "25899L";
+  standard_test_4 [7L] "7L";
+  standard_test_4 [0L; 1L] "0L_1L";
+  standard_test_4 [9223372036854775808L; 1L] "2pow63_1L";
+  standard_test_4 (genalea 1000L) "e3_bits";
+  standard_test_4 (genalea 2000L) "2e3_bits";
+  standard_test_4 (genalea 3000L) "3e3_bits";
+  (* standard_test_4 (genalea 10000L) "e4_bits"; *)
+  
+  (* Compressions différentes entre par arbre et par liste à partir d'environ 3000 bits.
+     Par exemple, le bigint suivant (sur 2999 bits) donne deux compressions différentes :
 
-let b = [25899L];; (* = 25899 *)
-print_bigint b;;
-let () = bigintToDot "25899L_LDV" b true;;
-let () = bigintToDot "25899L_ADV" b false;;
-printf "\n";;
+  [-7397863826597483745; -7770261156321738754; 1236646854865449075; -5190928585146045899;
+  -4992808975050723075; -5741163498365487965; 7946033078052947069; -2800735776790153635;
+  -3918936533814437455; -8998001415999952448; -6294537310539991044; -7668792850197517654;
+  8563250432377705112; -552875915331482884; -6668039009565253450; -881992044850089135;
+  -5531170330478518348; -3552442076188947852; 5565918363508105768; -9161973353537052590;
+  -5502829082253087124; -3043477099894200854; 4834271741461236247; 3176104119376014797;
+  8173290540801592168; -4800686476214529944; 2343392459435193004; -7177155205930299855;
+  8863579622411367543; 7547174846486908704; 7196820598621166457; 1761421479576635729;
+  -6805764409314456464; -8737641954624191063; -3284744692263401815; 1879820879237403955;
+  -1170213225930205699; -5764086671883100520; -6879513556585695580; -4857462984785588336;
+  6258772459160726337; -1049649545095147934; -8986565433282590063; 3399141512103088421;
+  -1858961850604665463; 7974041216154821691; 31402785079399147]
+  *)
+;;
 
-(*
-let b = [7L];; (* = 7 *)
-print_bigint b;;
-let () = bigintToDot "7L_LDV" b true;;
-let () = bigintToDot "7L_ADV" b false;;
-printf "\n";;
+let main () =
+  (* tests_1_2 (); *)
+  (* tests_3 (); *)
+  tests_4 ()
+;;
 
-let b = [0L; 1L];; (* = 2^64 *)
-print_bigint b;;
-let () = bigintToDot "0L_1L_LDV" b true;;
-let () = bigintToDot "0L_1L_ADV" b false;;
-printf "\n";;
-
-let b = [9223372036854775808L; 1L];; (* = 2^63 + 2^64 *)
-print_bigint b;;
-let () = bigintToDot "2pow63_1L_LDV" b true;;
-let () = bigintToDot "2pow63_1L_ADV" b false;;
-printf "\n";;
-
-let b = [-4077520903251697734L; 3725098519587869504L];; (* = 68715939040191755864311453423018172346 *)
-print_bigint b;;
-let () = bigintToDot "126bits_LDV" b true;;
-let () = bigintToDot "126bits_ADV" b false;;
-printf "\n";;
-
-let b = genalea 1000L;;
-print_bigint b;;
-let () = bigintToDot "e3_bits_LDV" b true;;
-let () = bigintToDot "e3_bits_ADV" b false;;
-printf "\n";;
-
-let b = genalea 2000L;;
-print_bigint b;;
-
-let a = decomposition b;;
-(* let listef = liste_feuilles a;; *)
-printf "taille = %d\n" (List.length a);;
-let () = List.iter (printf "%b ") a;;
-printf "\n";;
-
-let () = bigintToDot "2e3_bits_LDV" b true;;
-let () = bigintToDot "2e3_bits_ADV" b false;;
-printf "\n";;
-
-(* Les fichiers dots des arbres compressés ne sont pas identiques... *)
-let b = genalea 3000L;;
-print_bigint b;;
-let () = bigintToDot "3e3_bits_LDV" b true;;
-let () = bigintToDot "3e3_bits_ADV" b false;;
-printf "\n";;
-*)
-(* Le test suivant met une 20aine de secondes à générer les dots. *)
-(* Les fichiers dots des arbres compressés ne sont pas identiques... *)
-(*
-let b = genalea 10000L;;
-print_bigint b;;
-let () = bigintToDot "e4_bits_LDV" b true;;
-let () = bigintToDot "e4_bits_ADV" b false;;
-printf "\n"
-*)
-
-(* Compressions différentes pour, par exemple, le bigint suivant (sur 2999 bits) :
-
-[-7397863826597483745; -7770261156321738754; 1236646854865449075; -5190928585146045899;
--4992808975050723075; -5741163498365487965; 7946033078052947069; -2800735776790153635;
--3918936533814437455; -8998001415999952448; -6294537310539991044; -7668792850197517654;
-8563250432377705112; -552875915331482884; -6668039009565253450; -881992044850089135;
--5531170330478518348; -3552442076188947852; 5565918363508105768; -9161973353537052590;
--5502829082253087124; -3043477099894200854; 4834271741461236247; 3176104119376014797;
-8173290540801592168; -4800686476214529944; 2343392459435193004; -7177155205930299855;
-8863579622411367543; 7547174846486908704; 7196820598621166457; 1761421479576635729;
--6805764409314456464; -8737641954624191063; -3284744692263401815; 1879820879237403955;
--1170213225930205699; -5764086671883100520; -6879513556585695580; -4857462984785588336;
-6258772459160726337; -1049649545095147934; -8986565433282590063; 3399141512103088421;
--1858961850604665463; 7974041216154821691; 31402785079399147]
-
-*)
-
+main ();;
 print_string "Done.\n";;
